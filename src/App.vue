1c6b618651a6da95bd21ce35f99a41c2615aa651
Alex Maramaldo <alex@osbox.com.br>
2019-12-11 21:18:47 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SearchField msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import SearchField from "./components/SearchField.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    SearchField
   }
 };
 </script>
---
+++ src/components/SearchField.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="content">
+    <md-autocomplete v-model="value" :md-options="suggestions" @md-changed="getSuggestions">
+      <label>Suggestion</label>
+    </md-autocomplete>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "SearchField",
+  props: {
+    msg: String
+  },
+  data: () => ({
+    value: null,
+    suggestionList: [],
+    suggestions: []
+  }),
+  methods: {
+    getSuggestions(searchTerm) {
+      var self = this;
+      window.setTimeout(() => {
+        const term = searchTerm.toLowerCase();
+        axios.get("http://localhost:8000/suggestions/index/" + term).then(
+          response => {
+            this.suggestionList = response.data.data;
+            this.suggestions = [];
+            this.suggestionList.forEach(function(item) {
+              console.log(item);
+              self.suggestions.push(item.name);
+            });
+            console.log(this.suggestions);
+          },
+          error => {
+            console.log("error " + error);
+          }
+        );
+      }, 500);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.content {
+  padding-left: 20%;
+  width: 80%;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
